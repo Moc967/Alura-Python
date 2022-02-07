9a012417607a71653a11cdd8353385e3e24b7c8a
Magnovaldo Carneiro <magnovaldo.carneiro@gmail.com>
2022-02-07 16:08:19 -0300

    Jogo da Advinhação

--- advinhacao.py
+++ advinhacao.py
@@ -14,18 +14,18 @@ while rodada <= totalDeTentativas:
     print("Tentativa {} de {}".format(rodada, totalDeTentativas))
     chute = int(input("Escolha um número entre 1 e 60 \n"))
     rodada = rodada + 1
-    if chute > maxRange:
+    if chute < 1 or chute > maxRange:
         print(chute, "está fora da faixa permitida\n")
         rodada = rodada - 1
+        continue
+    if chute > numeroSecreto:
+        print("O número informado é maior que o número secreto")
+    elif chute < numeroSecreto:
+        print("O número informado é menor que o número secreto")
     else:
-        if chute > numeroSecreto:
-            print("O número informado é maior que o número secreto")
-        elif chute < numeroSecreto:
-            print("O número informado é menor que o número secreto")
-        else:
-            print("Parabens!! Voce acertou o número secreto")
-            break
-        if rodada == 6:
-            print("\nVoce realizou todas as tentativas e não acertou o número secreto.")
-        else:
-            print("Tente novamente\n")
+        print("Parabens!! Voce acertou o número secreto")
+        break
+    if rodada == 6:
+        print("\nVoce realizou todas as tentativas e não acertou o número secreto.")
+    else:
+        print("Tente novamente\n")
