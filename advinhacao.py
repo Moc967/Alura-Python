2266ab1b1dd23778655b8e91c9da5a727893b6be
Magnovaldo Carneiro <magnovaldo.carneiro@gmail.com>
2022-02-07 14:52:14 -0300

    Jogo da Advinhação

--- advinhacao.py
+++ advinhacao.py
@@ -5,25 +5,27 @@ print("*** Bem vindo ao jogo de Advinhação ***")
 print("***************************************")
 print("\nVocê terá cinco chances de acertar o número sorteado.\nBoa sorte!!\n")
 
-maxrange = 60
-cont = 5
-numeroSecreto = random.randrange(1, maxrange)
-while cont > 0:
-    cont = cont - 1
-    chute = int(input("Escolha um número entre 1 e 60 \n"))
+maxRange = 60
+rodada = 1
+totalDeTentativas = 5
+numeroSecreto = random.randrange(1, maxRange)
 
-    if chute > maxrange:
+while rodada <= totalDeTentativas:
+    print("Tentativa {} de {}".format(rodada, totalDeTentativas))
+    chute = int(input("Escolha um número entre 1 e 60 \n"))
+    rodada = rodada + 1
+    if chute > maxRange:
         print(chute, "está fora da faixa permitida\n")
-        cont = cont + 1
+        rodada = rodada - 1
     else:
         if chute > numeroSecreto:
             print("O número informado é maior que o número secreto")
         elif chute < numeroSecreto:
-                print("O número informado é menor que o número secreto")
-            else:
-                print("Parabens!! Voce acertou o número secreto")
-                break
-        if cont == 0:
-            print("Voce realizou todas as tentativas, e não acertou o número secreto.")
+            print("O número informado é menor que o número secreto")
+        else:
+            print("Parabens!! Voce acertou o número secreto")
+            break
+        if rodada == 6:
+            print("\nVoce realizou todas as tentativas e não acertou o número secreto.")
         else:
             print("Tente novamente\n")
