235dbc2bc1e66029c3d6f66ebbeb1a7253933961
Magnovaldo Carneiro <magnovaldo.carneiro@gmail.com>
2022-02-07 14:20:41 -0300

    Jogo da Advinhação

--- advinhacao.py
+++ advinhacao.py
@@ -11,15 +11,20 @@ numeroSecreto = random.randrange(1, maxrange)
 while cont > 0:
     cont = cont - 1
     chute = int(input("Escolha um número entre 1 e 60 \n"))
+
     if chute > maxrange:
         print(chute, "está fora da faixa permitida\n")
         cont = cont + 1
     else:
-        if chute == numeroSecreto:
-            print("Parabens!! Voce acertou o número secreto")
-            break
+        if chute > numeroSecreto:
+            print("O número informado é maior que o número secreto")
         else:
-            if cont == 0:
-                print("Voce realizou todas as tentativas, e não acertou o número secreto.")
+            if chute < numeroSecreto:
+                print("O número informado é menor que o número secreto")
             else:
-                print("Voce não acertou. Tente novamente\n")
+                print("Parabens!! Voce acertou o número secreto")
+                break
+        if cont == 0:
+            print("Voce realizou todas as tentativas, e não acertou o número secreto.")
+        else:
+            print("Tente novamente\n")
