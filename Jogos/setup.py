8c7b2ded17bc1ba60c437ba1fb2099700296a428
Magnovaldo Carneiro <magnovaldo.carneiro@gmail.com>
2022-02-14 12:14:33 -0300

    Update in forca game

---
+++ Jogos/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Code+',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Open Source',
+    author='Magnovaldo Carneiro',
+    author_email='magnovaldo.carneiro@gmail.com',
+    description='Apps developed during Python training'
+)
